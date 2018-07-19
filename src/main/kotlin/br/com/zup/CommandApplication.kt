a0a8efde99fe4ea479f1493d7debb17463bfb5b3
celfons <marcel.resende@yahoo.com.br>
2018-07-19 14:52:42 -0300

    fix

--- src/main/kotlin/br/com/zup/CommandApplication.kt
+++ src/main/kotlin/br/com/zup/CommandApplication.kt
@@ -1,11 +1,14 @@
 package br.com.zup
 
+import br.com.zup.integration.Integration
+import br.com.zup.repository.ServiceMongoRepository
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
 
 @SpringBootApplication
 @Configuration
@@ -14,7 +17,21 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @ComponentScan(
         basePackages = ["br.com.zup"]
 )
-open class CommandApplication
+open class CommandApplication(
+        private val integration: Integration,
+        private var serviceMongoRepository: ServiceMongoRepository
+){
+    @Scheduled(fixedRate = 36000)
+    fun run() {
+
+        val listServices = serviceMongoRepository.findAll().sortedBy { it.order }
+
+        listServices.forEach{
+            integration.integrationService(it)
+        }
+
+    }
+}
 
 fun main(args: Array<String>) {
 
